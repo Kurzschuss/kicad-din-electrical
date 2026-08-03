d1bb5ccd1e3bcdf4f935e03b05075010a9fab140
Uwe <uwezimprich@googlemail.com>
2026-08-03 22:53:50 +0200

    3D-Modellpfad auf Z_-Bibliothek ausrichten

--- tools/windows/register_kicad_z_paths.ps1
+++ tools/windows/register_kicad_z_paths.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference = 'Stop'
 
 $managedVariables = [ordered]@{
     KICAD_Z_ROOT_DIR         = $RootDirectory
-    KICAD_Z_3DMODEL_DIR      = Join-Path $RootDirectory '3dmodels'
+    KICAD_Z_3DMODEL_DIR      = Join-Path $RootDirectory '3dmodels\Z_3DModell.3dshapes'
     KICAD_Z_3RDPARTY_DIR     = Join-Path $RootDirectory '3rdparty'
     KICAD_Z_DESIGN_BLOCK_DIR = Join-Path $RootDirectory 'designblocks'
     KICAD_Z_FOOTPRINT_DIR    = Join-Path $RootDirectory 'footprints'
