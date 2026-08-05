9588aec2a8488a813b27090aadc4c55ac9c351c8
Uwe <uwezimprich@googlemail.com>
2026-08-05 14:02:13 +0200

    Entwicklungsnavigator exportieren

--- tools/z_cockpit/__init__.py
+++ tools/z_cockpit/__init__.py
@@ -1,5 +1,11 @@
 """Gemeinsame Bausteine für das Z_Cockpit."""
 
+from .development_navigator import (
+    NavigatorRecommendation,
+    blocked_tasks,
+    development_navigator_html,
+    recommended_work,
+)
 from .pages import DEFAULT_PAGES, PageSpec, page_by_id
 from .project_dashboard import (
     DashboardTask,
@@ -15,16 +21,20 @@ from .security_status import SecurityItem, collect_security_status
 __all__ = [
     "DEFAULT_PAGES",
     "DashboardTask",
+    "NavigatorRecommendation",
     "PageSpec",
     "ProjectState",
     "SecurityItem",
     "StatusItem",
+    "blocked_tasks",
     "collect_project_status",
     "collect_security_status",
+    "development_navigator_html",
     "load_project_state",
     "next_dashboard_tasks",
     "next_tasks_html",
     "page_by_id",
     "progress_bar_html",
     "project_progress_html",
+    "recommended_work",
 ]
