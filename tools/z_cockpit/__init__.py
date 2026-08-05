3715ca5b468fbf8cdfaa78a25462ed717ce92dc0
Uwe <uwezimprich@googlemail.com>
2026-08-05 11:37:48 +0200

    Projektstatus-API exportieren

--- tools/z_cockpit/__init__.py
+++ tools/z_cockpit/__init__.py
@@ -1,5 +1,12 @@
 """Gemeinsame Bausteine für das Z_Cockpit."""
 
 from .pages import DEFAULT_PAGES, PageSpec, page_by_id
+from .project_status import StatusItem, collect_project_status
 
-__all__ = ["DEFAULT_PAGES", "PageSpec", "page_by_id"]
+__all__ = [
+    "DEFAULT_PAGES",
+    "PageSpec",
+    "StatusItem",
+    "collect_project_status",
+    "page_by_id",
+]
