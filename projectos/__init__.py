91a39e6102d34eda8bb9fb5a1e94708ee0326290
Uwe <uwezimprich@googlemail.com>
2026-08-06 07:52:04 +0200

    feat(auth): AP-0028 öffentliche Exporte ergänzen

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -1,6 +1,13 @@
 """ProjectOS-Kernpaket für kicad-din-electrical."""
 
 from .application import Command, LocalCommandBus, LocalQueryBus, Query
+from .authorization import (
+    AuthorizationContext,
+    AuthorizationResult,
+    AuthorizationService,
+    ExceptionRight,
+    Role,
+)
 from .events import DomainEvent, DomainEventCollector, LocalEventBus
 from .identifiers import BusinessId, CorrelationId, ObjectId
 from .repositories import InMemoryRepository, Repository, RepositoryEntity, RepositoryRecord
@@ -9,11 +16,15 @@ from .runtime import RuntimeInfo, create_runtime_info
 from .validation import ValidationProfile, ValidationResult, ValidationRule, Validator
 
 __all__ = [
+    "AuthorizationContext",
+    "AuthorizationResult",
+    "AuthorizationService",
     "BusinessId",
     "Command",
     "CorrelationId",
     "DomainEvent",
     "DomainEventCollector",
+    "ExceptionRight",
     "InMemoryRepository",
     "LocalCommandBus",
     "LocalEventBus",
@@ -26,6 +37,7 @@ __all__ = [
     "RepositoryRecord",
     "Result",
     "ResultMessage",
+    "Role",
     "RuntimeInfo",
     "ValidationProfile",
     "ValidationResult",
@@ -33,4 +45,4 @@ __all__ = [
     "Validator",
     "create_runtime_info",
 ]
-__version__ = "0.7.0"
+__version__ = "0.8.0"
