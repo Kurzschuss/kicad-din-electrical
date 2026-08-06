d2a831de88cb013418fcd77f14db97b0e20bb99a
Uwe <uwezimprich@googlemail.com>
2026-08-06 07:39:50 +0200

    AP-0023 Result-Framework exportieren

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -1,13 +1,17 @@
 """ProjectOS-Kernpaket für kicad-din-electrical."""
 
 from .identifiers import BusinessId, CorrelationId, ObjectId
+from .results import MessageSeverity, Result, ResultMessage
 from .runtime import RuntimeInfo, create_runtime_info
 
 __all__ = [
     "BusinessId",
     "CorrelationId",
+    "MessageSeverity",
     "ObjectId",
+    "Result",
+    "ResultMessage",
     "RuntimeInfo",
     "create_runtime_info",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
