0a8f4f1844dd92f6e4c2800b9535cb964881f73f
Uwe <uwezimprich@googlemail.com>
2026-08-06 07:59:15 +0200

    feat(mcb): AP-0031 öffentliche MCB-API ergänzen

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -2,15 +2,10 @@
 
 from .application import Command, LocalCommandBus, LocalQueryBus, Query
 from .audit import AuditEntry, InMemoryAuditRepository
-from .authorization import (
-    AuthorizationContext,
-    AuthorizationResult,
-    AuthorizationService,
-    ExceptionRight,
-    Role,
-)
+from .authorization import AuthorizationContext, AuthorizationResult, AuthorizationService, ExceptionRight, Role
 from .events import DomainEvent, DomainEventCollector, LocalEventBus
 from .identifiers import BusinessId, CorrelationId, ObjectId
+from .mcb import BreakingCapacity, MCB, NominalCurrent, PoleCount, RatedVoltage, TripCharacteristic, create_mcb_validation_profile, validate_mcb
 from .repositories import InMemoryRepository, Repository, RepositoryEntity, RepositoryRecord
 from .results import MessageSeverity, Result, ResultMessage
 from .runtime import RuntimeInfo, create_runtime_info
@@ -18,39 +13,14 @@ from .simulation import SimulationClock, SimulationContext, SimulationTrace, Sim
 from .validation import ValidationProfile, ValidationResult, ValidationRule, Validator
 
 __all__ = [
-    "AuditEntry",
-    "AuthorizationContext",
-    "AuthorizationResult",
-    "AuthorizationService",
-    "BusinessId",
-    "Command",
-    "CorrelationId",
-    "DomainEvent",
-    "DomainEventCollector",
-    "ExceptionRight",
-    "InMemoryAuditRepository",
-    "InMemoryRepository",
-    "LocalCommandBus",
-    "LocalEventBus",
-    "LocalQueryBus",
-    "MessageSeverity",
-    "ObjectId",
-    "Query",
-    "Repository",
-    "RepositoryEntity",
-    "RepositoryRecord",
-    "Result",
-    "ResultMessage",
-    "Role",
-    "RuntimeInfo",
-    "SimulationClock",
-    "SimulationContext",
-    "SimulationTrace",
-    "SimulationTraceEntry",
-    "ValidationProfile",
-    "ValidationResult",
-    "ValidationRule",
-    "Validator",
-    "create_runtime_info",
+    "AuditEntry", "AuthorizationContext", "AuthorizationResult", "AuthorizationService",
+    "BreakingCapacity", "BusinessId", "Command", "CorrelationId", "DomainEvent",
+    "DomainEventCollector", "ExceptionRight", "InMemoryAuditRepository", "InMemoryRepository",
+    "LocalCommandBus", "LocalEventBus", "LocalQueryBus", "MCB", "MessageSeverity",
+    "NominalCurrent", "ObjectId", "PoleCount", "Query", "RatedVoltage", "Repository",
+    "RepositoryEntity", "RepositoryRecord", "Result", "ResultMessage", "Role", "RuntimeInfo",
+    "SimulationClock", "SimulationContext", "SimulationTrace", "SimulationTraceEntry",
+    "TripCharacteristic", "ValidationProfile", "ValidationResult", "ValidationRule", "Validator",
+    "create_mcb_validation_profile", "create_runtime_info", "validate_mcb",
 ]
-__version__ = "0.10.0"
+__version__ = "0.11.0"
