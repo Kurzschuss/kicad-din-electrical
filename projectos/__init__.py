29265e6ea98d958919e94f7f1950312d179069e5
Uwe <uwezimprich@googlemail.com>
2026-08-06 08:50:55 +0200

    feat(projectos): AP-0034 Workflow öffentlich exportieren

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -13,19 +13,27 @@ from .results import MessageSeverity, Result, ResultMessage
 from .runtime import RuntimeInfo, create_runtime_info
 from .simulation import SimulationClock, SimulationContext, SimulationTrace, SimulationTraceEntry
 from .validation import ValidationProfile, ValidationResult, ValidationRule, Validator
+from .workflows import (
+    PERM_PROTECTION_REGISTER,
+    REGISTER_PROTECTION_PAIR,
+    ProtectionRegistrationResult,
+    RegisterProtectionPairHandler,
+)
 
 __all__ = [
     "AuditEntry", "AuthorizationContext", "AuthorizationResult", "AuthorizationService",
     "BreakingCapacity", "BusinessId", "Command", "CorrelationId", "DomainEvent",
     "DomainEventCollector", "ExceptionRight", "InMemoryAuditRepository", "InMemoryRepository",
     "LocalCommandBus", "LocalEventBus", "LocalQueryBus", "MCB", "MessageSeverity",
-    "NominalCurrent", "ObjectId", "PoleCount", "ProtectionDevicePair",
-    "ProtectionValidationResult", "Query", "RCCB", "RCCBPoleCount", "RCCBRatedVoltage",
-    "RCCBType", "RatedCurrent", "RatedVoltage", "Repository", "RepositoryEntity",
-    "RepositoryRecord", "ResidualCurrent", "Result", "ResultMessage", "Role", "RuntimeInfo",
-    "SimulationClock", "SimulationContext", "SimulationTrace", "SimulationTraceEntry",
-    "TripCharacteristic", "ValidationProfile", "ValidationResult", "ValidationRule", "Validator",
-    "create_mcb_validation_profile", "create_rccb_validation_profile", "create_runtime_info",
-    "validate_mcb", "validate_protection_pair", "validate_rccb",
+    "NominalCurrent", "ObjectId", "PERM_PROTECTION_REGISTER", "PoleCount",
+    "ProtectionDevicePair", "ProtectionRegistrationResult", "ProtectionValidationResult", "Query",
+    "RCCB", "RCCBPoleCount", "RCCBRatedVoltage", "RCCBType", "REGISTER_PROTECTION_PAIR",
+    "RatedCurrent", "RatedVoltage", "RegisterProtectionPairHandler", "Repository",
+    "RepositoryEntity", "RepositoryRecord", "ResidualCurrent", "Result", "ResultMessage",
+    "Role", "RuntimeInfo", "SimulationClock", "SimulationContext", "SimulationTrace",
+    "SimulationTraceEntry", "TripCharacteristic", "ValidationProfile", "ValidationResult",
+    "ValidationRule", "Validator", "create_mcb_validation_profile",
+    "create_rccb_validation_profile", "create_runtime_info", "validate_mcb",
+    "validate_protection_pair", "validate_rccb",
 ]
-__version__ = "0.13.0"
+__version__ = "0.14.0"
