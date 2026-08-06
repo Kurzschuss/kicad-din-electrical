f6ce29e07c35e0032340f503377867f3b83eccaf
Uwe <uwezimprich@googlemail.com>
2026-08-06 14:07:37 +0200

    AP-0071 öffentliche API und Version aktualisieren

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -14,6 +14,7 @@ from .kicad_library_snapshot import KiCadCompleteSnapshotBuilder, KiCadCompleteS
 from .kicad_library_tables import KiCadLibraryTable, KiCadLibraryTableEntry, KiCadLibraryTableParser, KiCadLibraryTableType, KiCadVariableContext
 from .kicad_library_validation import KiCadAssetRequirement, KiCadLibraryItemSnapshot, KiCadLibraryValidationResult, KiCadLibraryValidator, KiCadTargetRequirements, KiCadValidationFinding, KiCadValidationSeverity
 from .kicad_native_snapshot import KiCadNativeSnapshotBuilder, KiCadSnapshotBuildResult, NativeKiCadSource
+from .kicad_project_validation import KiCadProjectValidationPipeline, KiCadProjectValidationResult, KiCadProjectValidationTarget
 from .manufacturer import Manufacturer, ManufacturerReference, ManufacturerStatus, ProductSeries, ensure_unique_series_name
 from .manufacturer_product import ManufacturerProduct, ProductIdentifier, ProductIdentifierType, ProductStatus, ensure_unique_product_identifiers
 from .mcb import BreakingCapacity, MCB, NominalCurrent, PoleCount, RatedVoltage, TripCharacteristic, create_mcb_validation_profile, validate_mcb
@@ -50,4 +51,4 @@ from .validation import ValidationProfile, ValidationResult, ValidationRule, Val
 from .workflows import PERM_PROTECTION_REGISTER, REGISTER_PROTECTION_PAIR, ProtectionRegistrationResult, RegisterProtectionPairHandler
 
 __all__ = [name for name in globals() if not name.startswith("_")]
-__version__ = "0.48.0"
+__version__ = "0.49.0"
