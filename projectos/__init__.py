a2217d954a301b836ec0fb76eeb67a621d379dfb
Uwe <uwezimprich@googlemail.com>
2026-08-06 09:53:17 +0200

    Export AP-0041 outbox API

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -14,6 +14,7 @@ from .device_persistence import (
 from .events import DomainEvent, DomainEventCollector, LocalEventBus
 from .identifiers import BusinessId, CorrelationId, ObjectId
 from .mcb import BreakingCapacity, MCB, NominalCurrent, PoleCount, RatedVoltage, TripCharacteristic, create_mcb_validation_profile, validate_mcb
+from .outbox import AtomicOutboxResult, OutboxMessage, SQLiteOutboxRepository, add_with_outbox
 from .protection import ProtectionDevicePair, ProtectionValidationResult, validate_protection_pair
 from .rccb import RCCB, RCCBPoleCount, RCCBRatedVoltage, RCCBType, RatedCurrent, ResidualCurrent, create_rccb_validation_profile, validate_rccb
 from .release import ReleaseManifest, SemanticVersion, VersionBump
@@ -32,22 +33,23 @@ from .workflows import (
 )
 
 __all__ = [
-    "AtomicPersistenceResult", "AuditEntry", "AuthorizationContext", "AuthorizationResult",
-    "AuthorizationService", "BreakingCapacity", "BusinessId", "Command", "CorrelationId",
-    "DomainEvent", "DomainEventCollector", "ExceptionRight", "InMemoryAuditRepository",
-    "InMemoryRepository", "LocalCommandBus", "LocalEventBus", "LocalQueryBus", "MCB",
-    "MessageSeverity", "NominalCurrent", "ObjectId", "PERM_PROTECTION_REGISTER", "PoleCount",
-    "ProtectionDevicePair", "ProtectionRegistrationResult", "ProtectionValidationResult", "Query",
-    "RCCB", "RCCBPoleCount", "RCCBRatedVoltage", "RCCBType", "REGISTER_PROTECTION_PAIR",
+    "AtomicOutboxResult", "AtomicPersistenceResult", "AuditEntry", "AuthorizationContext",
+    "AuthorizationResult", "AuthorizationService", "BreakingCapacity", "BusinessId", "Command",
+    "CorrelationId", "DomainEvent", "DomainEventCollector", "ExceptionRight",
+    "InMemoryAuditRepository", "InMemoryRepository", "LocalCommandBus", "LocalEventBus",
+    "LocalQueryBus", "MCB", "MessageSeverity", "NominalCurrent", "ObjectId", "OutboxMessage",
+    "PERM_PROTECTION_REGISTER", "PoleCount", "ProtectionDevicePair",
+    "ProtectionRegistrationResult", "ProtectionValidationResult", "Query", "RCCB",
+    "RCCBPoleCount", "RCCBRatedVoltage", "RCCBType", "REGISTER_PROTECTION_PAIR",
     "RatedCurrent", "RatedVoltage", "RegisterProtectionPairHandler", "ReleaseManifest",
     "Repository", "RepositoryEntity", "RepositoryRecord", "ResidualCurrent", "Result",
     "ResultMessage", "Role", "RuntimeInfo", "SQLiteAuditRepository", "SQLiteJsonRepository",
-    "SQLiteRepositoryConfig", "SQLiteUnitOfWork", "SemanticVersion", "SimulationClock",
-    "SimulationContext", "SimulationTrace", "SimulationTraceEntry", "TripCharacteristic",
-    "ValidationProfile", "ValidationResult", "ValidationRule", "Validator", "VersionBump",
-    "add_with_audit", "create_mcb_sqlite_repository", "create_mcb_validation_profile",
-    "create_rccb_sqlite_repository", "create_rccb_validation_profile", "create_runtime_info",
-    "decode_mcb", "decode_rccb", "encode_mcb", "encode_rccb", "validate_mcb",
-    "validate_protection_pair", "validate_rccb",
+    "SQLiteOutboxRepository", "SQLiteRepositoryConfig", "SQLiteUnitOfWork", "SemanticVersion",
+    "SimulationClock", "SimulationContext", "SimulationTrace", "SimulationTraceEntry",
+    "TripCharacteristic", "ValidationProfile", "ValidationResult", "ValidationRule", "Validator",
+    "VersionBump", "add_with_audit", "add_with_outbox", "create_mcb_sqlite_repository",
+    "create_mcb_validation_profile", "create_rccb_sqlite_repository",
+    "create_rccb_validation_profile", "create_runtime_info", "decode_mcb", "decode_rccb",
+    "encode_mcb", "encode_rccb", "validate_mcb", "validate_protection_pair", "validate_rccb",
 ]
-__version__ = "0.18.0"
+__version__ = "0.19.0"
