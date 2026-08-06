69139f5b2728561679afc99819590225d77cc28d
Uwe <uwezimprich@googlemail.com>
2026-08-06 07:34:55 +0200

    feat(projectos): AP-0021 ausführbares Paket anlegen

---
+++ projectos/__init__.py
@@ -0,0 +1,6 @@
+"""ProjectOS-Kernpaket für kicad-din-electrical."""
+
+from .runtime import RuntimeInfo, create_runtime_info
+
+__all__ = ["RuntimeInfo", "create_runtime_info"]
+__version__ = "0.1.0"
