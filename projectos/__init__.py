5034b13bcc7897eed3a409458f4ccfa89ff54ed0
Uwe <uwezimprich@googlemail.com>
2026-08-06 08:05:00 +0200

    feat(projectos): AP-0033 Schutzgeräte-API exportieren

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -6,6 +6,7 @@ from .authorization import AuthorizationContext, AuthorizationResult, Authorizat
 from .events import DomainEvent, DomainEventCollector, LocalEventBus
 from .identifiers import BusinessId, CorrelationId, ObjectId
 from .mcb import BreakingCapacity, MCB, NominalCurrent, PoleCount, RatedVoltage, TripCharacteristic, create_mcb_validation_profile, validate_mcb
+from .protection import ProtectionDevicePair, ProtectionValidationResult, validate_protection_pair
 from .rccb import RCCB, RCCBPoleCount, RCCBRatedVoltage, RCCBType, RatedCurrent, ResidualCurrent, create_rccb_validation_profile, validate_rccb
 from .repositories import InMemoryRepository, Repository, RepositoryEntity, RepositoryRecord
 from .results import MessageSeverity, Result, ResultMessage
@@ -18,12 +19,13 @@ __all__ = [
     "BreakingCapacity", "BusinessId", "Command", "CorrelationId", "DomainEvent",
     "DomainEventCollector", "ExceptionRight", "InMemoryAuditRepository", "InMemoryRepository",
     "LocalCommandBus", "LocalEventBus", "LocalQueryBus", "MCB", "MessageSeverity",
-    "NominalCurrent", "ObjectId", "PoleCount", "Query", "RCCB", "RCCBPoleCount",
-    "RCCBRatedVoltage", "RCCBType", "RatedCurrent", "RatedVoltage", "Repository",
-    "RepositoryEntity", "RepositoryRecord", "ResidualCurrent", "Result", "ResultMessage",
-    "Role", "RuntimeInfo", "SimulationClock", "SimulationContext", "SimulationTrace",
-    "SimulationTraceEntry", "TripCharacteristic", "ValidationProfile", "ValidationResult",
-    "ValidationRule", "Validator", "create_mcb_validation_profile", "create_rccb_validation_profile",
-    "create_runtime_info", "validate_mcb", "validate_rccb",
+    "NominalCurrent", "ObjectId", "PoleCount", "ProtectionDevicePair",
+    "ProtectionValidationResult", "Query", "RCCB", "RCCBPoleCount", "RCCBRatedVoltage",
+    "RCCBType", "RatedCurrent", "RatedVoltage", "Repository", "RepositoryEntity",
+    "RepositoryRecord", "ResidualCurrent", "Result", "ResultMessage", "Role", "RuntimeInfo",
+    "SimulationClock", "SimulationContext", "SimulationTrace", "SimulationTraceEntry",
+    "TripCharacteristic", "ValidationProfile", "ValidationResult", "ValidationRule", "Validator",
+    "create_mcb_validation_profile", "create_rccb_validation_profile", "create_runtime_info",
+    "validate_mcb", "validate_protection_pair", "validate_rccb",
 ]
-__version__ = "0.12.0"
+__version__ = "0.13.0"
