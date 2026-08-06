7cf335307ff6b9669205b96d655b18dceeef0c2e
Uwe <uwezimprich@googlemail.com>
2026-08-06 14:25:40 +0200

    AP-0075: öffentliche Freigabeaudit-API

--- projectos/__init__.py
+++ projectos/__init__.py
@@ -16,6 +16,7 @@ from .kicad_library_validation import KiCadAssetRequirement, KiCadLibraryItemSna
 from .kicad_native_snapshot import KiCadNativeSnapshotBuilder, KiCadSnapshotBuildResult, NativeKiCadSource
 from .kicad_project_validation import KiCadProjectValidationPipeline, KiCadProjectValidationResult, KiCadProjectValidationTarget
 from .kicad_quality_gate import KiCadQualityGateFinding, KiCadQualityGateResult, KiCadQualityGateService, KiCadQualityPolicy, KiCadReleaseDecision
+from .kicad_release_audit import KiCadReleaseAuditRecord, SQLiteKiCadReleaseAuditRepository
 from .kicad_validation_history import KiCadValidationComparison, KiCadValidationHistoryRecord, SQLiteKiCadValidationHistoryRepository
 from .kicad_validation_search import KiCadValidationSearchFilter, KiCadValidationSearchPage, KiCadValidationSearchService, KiCadValidationTrend
 from .manufacturer import Manufacturer, ManufacturerReference, ManufacturerStatus, ProductSeries, ensure_unique_series_name
@@ -54,4 +55,4 @@ from .validation import ValidationProfile, ValidationResult, ValidationRule, Val
 from .workflows import PERM_PROTECTION_REGISTER, REGISTER_PROTECTION_PAIR, ProtectionRegistrationResult, RegisterProtectionPairHandler
 
 __all__ = [name for name in globals() if not name.startswith("_")]
-__version__ = "0.52.0"
+__version__ = "0.53.0"
