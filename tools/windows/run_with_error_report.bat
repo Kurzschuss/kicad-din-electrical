59ce6578a4ec0502661312fc63213c77c10edb2d
Uwe <uwezimprich@googlemail.com>
2026-08-07 15:41:37 +0200

    fix: Windows Fehlerbericht-Befehle korrekt weiterreichen

--- tools/windows/run_with_error_report.bat
+++ tools/windows/run_with_error_report.bat
@@ -12,11 +12,20 @@ set "REPORT_LOG=%~2"
 shift
 shift
 
+set "COMMAND_ARGS="
+
+:collect_command_args
+if "%~1"=="" goto :run_command
+set "COMMAND_ARGS=%COMMAND_ARGS% "%~1""
+shift
+goto :collect_command_args
+
+:run_command
 python -m tools.run_with_error_report ^
   --title "%REPORT_TITLE%" ^
   --log "%REPORT_LOG%" ^
   --report "build\FEHLERBERICHT.md" ^
-  -- %*
+  -- %COMMAND_ARGS%
 
 set "RESULT=%ERRORLEVEL%"
 if not "%RESULT%"=="0" (
