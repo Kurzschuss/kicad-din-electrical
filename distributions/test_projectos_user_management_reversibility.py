ad82240c46defe1add8faf6b687eb00615a8deb7
Uwe <uwezimprich@googlemail.com>
2026-08-09 12:52:57 +0200

    test(projectos): Rollenzuweisungs-Beendigung in Reversibilitätsmatrix absichern

--- distributions/test_projectos_user_management_reversibility.py
+++ distributions/test_projectos_user_management_reversibility.py
@@ -20,6 +20,7 @@ def test_reversibility_matrix_is_fail_closed_and_only_weight_change_is_reversibl
         "permission_assigned",
         "permission_revoked",
         "project_role_assigned",
+        "project_role_assignment_terminated",
         "project_role_activated",
         "project_role_deactivated",
         "approval_requested",
